01e597fe977fbfede459f98f0662f96e6a299af4
heavy02011 <heavy02011>
2023-05-06 10:41:56 +0200

    feat: vis+ added

--- examples/rbx-simple-vis-depth.py
+++ examples/rbx-simple-vis-depth.py
@@ -9,15 +9,24 @@ SERIAL_DEV = '/dev/ttyUSB0'
 def visualize_depth(points):
     points = points.astype(np.int32)
 
-    min_depth = np.min(points[:, 2])
-    max_depth = np.max(points[:, 2])
-    normalized_depth = ((points[:, 2] - min_depth) / (max_depth - min_depth) * 255).astype(np.uint8)
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+    z_values = points[:, 2]
+
+    # min_depth = np.min(points[:, 2])
+    # max_depth = np.max(points[:, 2])
+    min_depth = 0 # np.min(z_values)
+    max_depth = 3000 #np.max(z_values)
+    # normalized_depth = ((points[:, 2] - min_depth) / (max_depth - min_depth) * 255).astype(np.uint8)
+    normalized_depth = ((z_values - min_depth) / (max_depth - min_depth) * 255).astype(np.uint8)
 
     image_size = (512, 512)
     image = np.zeros((image_size[0], image_size[1], 3), dtype=np.uint8)
 
-    x_img = ((points[:, 0] - np.min(points[:, 0])) / (np.max(points[:, 0]) - np.min(points[:, 0])) * (image_size[1] - 1)).astype(int)
-    y_img = ((points[:, 1] - np.min(points[:, 1])) / (np.max(points[:, 1]) - np.min(points[:, 1])) * (image_size[0] - 1)).astype(int)
+    # x_img = ((points[:, 0] - np.min(points[:, 0])) / (np.max(points[:, 0]) - np.min(points[:, 0])) * (image_size[1] - 1)).astype(int)
+    # y_img = ((points[:, 1] - np.min(points[:, 1])) / (np.max(points[:, 1]) - np.min(points[:, 1])) * (image_size[0] - 1)).astype(int)
+    x_img = ((x_values - np.min(x_values)) / (np.max(x_values) - np.min(x_values)) * (image_size[1] - 1)).astype(int)
+    y_img = ((y_values - np.min(y_values)) / (np.max(y_values) - np.min(y_values)) * (image_size[0] - 1)).astype(int)
 
     colormap = plt.get_cmap('jet')
     color_depth = (colormap(normalized_depth / 255)[:, :3] * 255).astype(np.uint8)
--- examples/rbx-simple-vis.py
+++ examples/rbx-simple-vis.py
@@ -3,18 +3,65 @@ import time
 import hybo
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.interpolate import griddata
+import matplotlib.tri as tri
 
 SERIAL_DEV = '/dev/ttyUSB0'
 
-def visualize_points(points):
+# simple xy-plot
+def visualize_points(points): 
     plt.clf()
-    plt.scatter(points[:, 0], points[:, 1], c='b', marker='o', s=10)
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+    z_values = points[:, 2]
+
+    #plt.scatter(points[:, 0], points[:, 1], c='b', marker='o', s=10)
+    plt.scatter(x_values, y_values, c='b', marker='o', s=10)
     plt.xlim(0, 400)
     plt.ylim(0, 400)
     plt.gca().set_aspect('equal', adjustable='box')
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('LIDAR Points Visualization')
+    plt.grid(True)
+    plt.pause(0.001)
+
+# colored xy-plot with z as depth
+def visualize_points2(points): 
+    plt.clf()
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+    z_values = points[:, 2] / 100. # mm --> cm
+
+    plt.scatter(x_values, y_values, c=z_values, marker='o', s=10, cmap='viridis')
+    plt.xlim(0, 400)
+    plt.ylim(0, 400)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('LIDAR Points Visualization')
+    plt.grid(True)
+    plt.colorbar(label='Z Value')
+    plt.pause(0.001)
+
+# 3 contour plot
+def visualize_points3(points):
+    plt.clf()
+    
+    x_values = points[:, 0]
+    y_values = points[:, 1]
+    z_values = points[:, 2]
+
+    triang = tri.Triangulation(x_values, y_values)
+    plt.tricontourf(triang, z_values, cmap=plt.get_cmap("viridis"))
+    
+    plt.xlim(0, 400)
+    plt.ylim(0, 400)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('LIDAR Points Visualization')
+    plt.colorbar(label='Z')
     plt.grid(True)
     plt.pause(0.001)
 
@@ -36,7 +83,8 @@ try:
             time_peak = raw_scan["time_peak"]
             new_scan  = raw_scan["points"]
 
-            visualize_points(np.array(new_scan))
+            visualize_points3(np.array(new_scan))
+            time.sleep(0.05)
 
 except KeyboardInterrupt:
     print("Visualization stopped.")
